35ac528d42ace838757291cc28e6ff2973370333
caroline burns <carolineburns@carolines-MacBook.local>
2019-02-05 15:29:26 -0700

    c in crud works' q:

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const dotenv = require('dotenv').config()
 app.use(cors())
 app.use(parser.json())
 
+// app.post('/my-post-route', (req, res) => {
+//     res.send(`You hit /my-post-route with the following message: ${req.body.message}`)
+//   })
+
+  knex('reminders')
+.insert({name: 'chores', description: 'sweep, bitch'})
 
 app.get('/', function(req, res, next){
      knex('reminders')
@@ -18,6 +24,20 @@ app.get('/', function(req, res, next){
      .catch((err) => {
          next(err)
      })
+})
+
+app.post('/reminders', (req, res, next) => {
+    knex('reminders').insert(req.body)
+    .then((rows) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      next(err);
+    });
+})
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
 })
 
 app.listen(port, function() {
